369652384fcfd19659bcda1e62507e7af64b1454
ndnghia <ndnghiadn@gmail.com>
2022-03-25 22:01:11 +0700

    update UI

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
-import { Table } from 'react-bootstrap'
+import { Accordion, Button } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td colSpan={2}>Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </Table>
+      <h1>Fastweigh-Admin</h1>
+      <Accordion alwaysOpen>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Apple&nbsp;<span style={{color: 'red' }}>(8)</span></Accordion.Header>
+          <Accordion.Body>
+            <div>
+              <Button variant="primary">Add</Button>
+              <Button variant="primary">Edit</Button>
+              <Button variant="danger">Delete</Button>
+            </div>
+            <span>Price: 18.200/kg</span>
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Accordion Item #2</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+            est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
     </div>
   );
 }
